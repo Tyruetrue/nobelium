46747c1f26329b01ef571a6b6edbe97a4820ff00
Tyruetrue <3546416816@qq.com>
2024-11-12 21:58:06 +0800

    update

--- components/Header.js
+++ components/Header.js
@@ -12,8 +12,7 @@ const NavBar = () => {
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true },
-    { id: 3, name: locale.NAV.TAGS, to: '/tags', show: true },
-    { id: 4, name: locale.NAV.LOG, to: '/log', show: true }
+    { id: 3, name: locale.NAV.LOG, to: '/log', show: true }
   ]
   return (
     <div className="flex-shrink-0">
